5add85a62cffb962d4d283ee8543c518c639f5d5
Alejo Borracci <borraccialejo@hotmail.es>
2024-05-21 16:50:30 -0300

    proyectos listos

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,27 +1,49 @@
 import Image from "next/image";
 import wearfashion from "@/../public/wearfashion.png";
-
+import drivers from "@/../public/drivers.png"
+import alk from "@/../public/alk.png"
 export default function Works() {
     return (
-      <div className="w-full bg-transparent h-screen flex flex-col">
-        <div className="relative w-2/4 h-2/5 bg-white ml-10 mt-14 overflow-hidden group">
+      <div className="w-full bg-transparent h-[200vh] flex flex-col">
+        <div className="relative w-2/4 h-[50vh] bg-white border border-white ml-10 mt-14 overflow-hidden group">
           <Image
             src={wearfashion}
             alt="wearfashion image"
             layout="fill"
             objectFit="cover"
-            className="transform transition-transform duration-500 ease-in-out group-hover:scale-110 group-hover:opacity-60"
+            className="transform transition-transform duration-500 ease-in-out group-hover:scale-110 group-hover:opacity-40"
           />
           <p className="absolute bottom-0 left-0 w-full text-black text-xl px-4 py-2 font-bold opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100">
-            WearFashion es un E-Commerce de ropa urbana que desarrollamos con mi
-            equipo para nuestro proyecto final en SoyHenry. En la web podemos
-            ver las diferentes prendas disponibles que tenemos en stock,
-            filtrarlas para ver solo las caracteristicas que estamos buscando y
-            si queremos realizar una compra tenemos la opcion de registrarnos
-            para poder acceder a una pasarela de pagos y realizar la compra
-            deseada.
+          WearFashion es una plataforma de E-Commerce de ropa urbana que desarrollamos con mi equipo como proyecto final en SoyHenry. En el sitio web, los usuarios pueden ver las diferentes prendas disponibles en stock y filtrarlas según las características que estén buscando. Si desean realizar una compra, tienen la opción de registrarse para acceder a una pasarela de pagos y completar la compra deseada. Además, la plataforma cuenta con un panel de administración que permite editar productos y acceder a las estadísticas del sitio.
           </p>
         </div>
+
+        <div className="relative w-2/4 h-[50vh] border border-white bg-white ml-auto mr-10 mt-14 overflow-hidden group">
+          <Image
+            src={drivers}
+            alt="Drivers image"
+            layout="fill"
+            objectFit="cover"
+            className="transform transition-transform duration-500 ease-in-out group-hover:scale-110 group-hover:opacity-40"
+          />
+          <p className="absolute bottom-0 left-0 w-full text-black text-xl px-4 py-2 font-bold opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100">
+          Drivers fue un proyecto individual que realicé en SoyHenry. Al ingresar, lo primero que vemos es la página de inicio o landing page, luego nos dirigimos a la página principal donde se muestran los distintos corredores almacenados en la base de datos. Los usuarios pueden filtrar estos corredores según equipos, edad y nombre. Además, es posible agregar nuevos corredores a través de un formulario. Por último, hay una sección "Acerca de mí", donde se presenta una breve descripción del proyecto y de mí como desarrollador.
+          </p>
+        </div>
+
+        <div className="relative w-2/4 h-[50vh] border border-white bg-white ml-10 mt-14 overflow-hidden group">
+          <Image
+            src={alk}
+            alt="ALK image"
+            layout="fill"
+            objectFit="cover"
+            className="transform transition-transform duration-500 ease-in-out group-hover:scale-110 group-hover:opacity-40"
+          />
+          <p className="absolute bottom-0 left-0 w-full text-black text-xl px-4 py-2 font-bold opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100">
+          ALK es un emprendimiento que desarrollamos con dos amigos, enfocado en ofrecer servicios para páginas web. Nuestras soluciones van desde el desarrollo de una página desde cero hasta la implementación de nuevas funciones para mejorar su sitio web existente. ALK eleva su página web al siguiente nivel al ofrecer soluciones rápidas, interfaces de usuario atractivas e intuitivas, basándonos en la excelencia técnica y el profesionalismo.
+          </p>
+        </div>
+        
       </div>
     );
 }
