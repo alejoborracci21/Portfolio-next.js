9f48ceb31e3ed4f7060b5b35c18b67f90a8a2aa6
Alejo Borracci <borraccialejo@hotmail.es>
2024-05-23 17:34:56 -0300

    image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,45 +20,50 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen h-[200vh] flex-col items-center mt-20">
-      <div className="flex flex-col items-center h-1/2 w-7/12">
-        <Image
-          src={image}
-          alt="foto"
-          width={200}
-          height={350}
-          className="rounded-full mt-8 border-2"
-        />
-        <p className="mt-7 w-full text-center">
-          ¡Hola! Soy Alejo, un apasionado
-          <span className="text-blue-500">
-            {" "}
-            Desarrollador Web Fullstack
-          </span>{" "}
-          especializado principalmente en
-          <span className="text-blue-500"> Front-end. </span>
-          Me encanta crear interfaces de usuario atractivas y funcionales, y
-          tengo un fuerte compromiso con la excelencia en cada proyecto en el
-          que participo. Estoy emocionado de poder contribuir con mi experiencia
-          y habilidades para llevar tu proyecto al siguiente nivel.
-        </p>
-        <div className="flex items-center w-[100%] ">
-          <a
-            href="https://www.linkedin.com/in/alejo-borracci-2323a6199/"
-            className=" w-[50%] text-center h-10 mt-10 text-blue-500 text-xl hover:text-green-500"
-          >
-            LinkedIn
-          </a>
-          <a href="https://github.com/alejoborracci21" className="w-[50%] text-center h-10 mt-10 text-blue-500 text-xl hover:text-green-500">
-            Github
-          </a>
+        <div className="flex flex-col items-center h-1/2 w-7/12">
+          <Image
+            src={image}
+            alt="foto"
+            width={200}
+            height={350}
+            className="rounded-full mt-8 border-2"
+          />
+
+          <p className="mt-7 w-full text-center">
+            ¡Hola! Soy Alejo, un apasionado
+            <span className="text-blue-500">
+              {" "}
+              Desarrollador Web Fullstack
+            </span>{" "}
+            especializado principalmente en
+            <span className="text-blue-500"> Front-end. </span>
+            Me encanta crear interfaces de usuario atractivas y funcionales, y
+            tengo un fuerte compromiso con la excelencia en cada proyecto en el
+            que participo. Estoy emocionado de poder contribuir con mi
+            experiencia y habilidades para llevar tu proyecto al siguiente
+            nivel.
+          </p>
+          <div className="flex items-center w-[100%] ">
+            <a
+              href="https://www.linkedin.com/in/alejo-borracci-2323a6199/"
+              className=" w-[50%] text-center h-10 mt-10 text-blue-500 text-xl hover:text-green-500"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://github.com/alejoborracci21"
+              className="w-[50%] text-center h-10 mt-10 text-blue-500 text-xl hover:text-green-500"
+            >
+              Github
+            </a>
+          </div>
+
+          <button className="mt-7 hover:text-emerald-400" onClick={downloadCV}>
+            Descargar CV
+          </button>
         </div>
 
-        <button className="mt-7 hover:text-emerald-400" onClick={downloadCV}>
-          Descargar CV
-        </button>
-      </div>
-
-      <Skills/>
+        <Skills />
     </main>
   );
 }
